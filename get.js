4685d2c140bb822b595f3a456445e24c34408db9
KevinJordil <kevin.jordil@outlook.com>
2021-04-22 15:48:16 +0200

    Send image when detect change

--- get.js
+++ get.js
@@ -2,8 +2,8 @@ const TelegramBot = require('node-telegram-bot-api');
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 var config = require('./config.json');
-var html2json = require('html2json').html2json;
-
+var looksSame = require('looks-same');
+const { exit } = require('process');
 
 (async () => {
   const browser = await puppeteer.launch({
@@ -12,39 +12,42 @@ var html2json = require('html2json').html2json;
 
   const page = await browser.newPage();
 
+  // Gaps login
   await page.goto('https://gaps.heig-vd.ch/consultation/controlescontinus/consultation.php?idst=15635');
-  console.log("Load page...")
-  await page.waitForSelector('table[style="background-image: url(/consultation/images/banner_heig-vd.png);"]', {
+  console.log("Load gaps login...")
+  await page.waitForSelector('select[id="user_idp"]', {
     visible: true,
   });
   console.log("Loaded !")
 
-  console.log("Check if login is needed")
-  if (await page.$('#wayf_submit_button') !== null) {
-    await page.select('#user_idp', 'https://aai-logon.hes-so.ch/idp/shibboleth')
-    console.log("Click on connect gaps button")
-    await page.click('#wayf_submit_button');
-    console.log("Load page...")
-    await page.waitForSelector('button', {
-      visible: true,
-    });
-    console.log("Loaded !")
+  console.log("Select HES-SO")
+  await page.select('#user_idp', 'https://aai-logon.hes-so.ch/idp/shibboleth')
 
-    if(page.url().includes("aai-logon")){
-      console.log("AAI login needed")
-      console.log("Filled the fields")
-      await page.type('#username', config.username);
-      await page.type('#password', config.password);
-      await page.click('#btn-submit');
-      console.log("Click on connect HES-SO button")
-      console.log("Load page...")
-      await page.waitForSelector('table[style="background-image: url(/consultation/images/banner_heig-vd.png);"]', {
-        visible: true,
-      });
-      console.log("Loaded !")
-    }
+  console.log("Click on connect button")
+  await page.click('#wayf_submit_button');
 
-  }
+
+  // AII login
+  console.log("Load page AAI login...")
+  await page.waitForSelector('button[id="btn-submit"]', {
+    visible: true,
+  });
+  console.log("Loaded !")
+
+  console.log("Filled the fields")
+  await page.type('#username', config.username);
+  await page.type('#password', config.password);
+
+  console.log("Click on connect HES-SO button")
+  await page.click('#btn-submit');
+
+
+  // Gaps grades
+  console.log("Load grades page...")
+  await page.waitForSelector('td[class="titreBox"]', {
+    visible: true,
+  });
+  console.log("Loaded !")
 
   console.log("load grades...")
   await page.waitForSelector('table[class="displayArray"]', {
@@ -52,50 +55,49 @@ var html2json = require('html2json').html2json;
   });
   console.log("Grades loaded !")
 
-  console.log("Save HTML")
+  console.log("Save HTML to image")
 
-  html = await page.$eval('table[class="displayArray"]', (element) => {
-    return element.innerHTML
-  })
-
-  fs.writeFileSync('./current.html', html);
+  grades = await page.$('div[id="result"]');
+  try {
+    await grades.screenshot({ path: './current.png' });
+  } catch(e) {
+    console.log("error :" + e)
+  }
 
   console.log("Saved !")
 
-
-  if(fs.existsSync('./old.html')){
+  if (fs.existsSync('./old.png')) {
     console.log("Compare with last time...")
-    var old = fs.readFileSync('./old.html', 'utf-8');
-    var current = fs.readFileSync('./current.html', 'utf-8');
+
+    looksSame('./current.png', './old.png', function(error, {equal}) {
+      if (!equal) {
+        console.log("Different from last time!")
+        console.log("Send notification...")
   
-    if(old === current){
-      console.log("Same from last time!")
-    } else {
-      console.log("Different from last time!")
-      let diff= "";
-      current.split('').forEach(function(val, i){
-        if (val != old.charAt(i))
-          diff += val ;         
+        const bot = new TelegramBot(config.telegram.token, { polling: true });
+        
+        const photo = fs.createReadStream('./current.png')
+        bot.sendPhoto(config.telegram.chatId, photo, { caption: "Il y a du changement" })
+  
+        console.log("Sended!")
+      } else {
+        console.log("Same from last time!")
+      }
+
+      fs.rename('./current.png', './old.png', () => {
+        console.log("Save current for next time!");
       });
 
-      let json = html2json(diff);
-      console.log("Send notification...")
+    });
 
-      const bot = new TelegramBot(config.telegram.token, {polling: true});
-    
-      bot.sendMessage(config.telegram.chatId, 
-        "Libellé : " + json.child[2].child[0].child[0].text + ", " +
-        "Note : " + json.child[5].child[0].text)
-      console.log("Sended!")
-    }
+
+
   } else {
-    console.log('First time use script!')
+    console.log('First time use script! Nothing to compare!')
   }
 
-  fs.rename('./current.html', './old.html', () => {
-    console.log("Save current for next time!");
-  });
-  
-  await browser.close();
 
+
+  await browser.close();
+  exit(0)
 })();
